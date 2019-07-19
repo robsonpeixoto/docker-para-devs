919cb56cb19f7837ef83117c1b1348f149f8cf89
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2019-07-19 15:49:54 -0300

    Upgrade all deps

--- 009-flask-mongo/api.py
+++ 009-flask-mongo/api.py
@@ -3,8 +3,7 @@ from flask_pymongo import PyMongo, ObjectId
 import random
 
 app = Flask(__name__)
-app.config['MONGO_HOST'] = 'mongo'
-app.config['MONGO_DBNAME'] = 'fbta'
+app.config['MONGO_URI'] = 'mongodb://mongo:27017/fbta'
 mongo = PyMongo(app)
 
 @app.route("/", methods=['POST'])
